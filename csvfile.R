2885b3ec255480423458626a27565a995a222dae
sahith_meesala <228w1a12a8@vrsec.ac.in>
2024-02-24 19:21:24 +0530

    csvfile

--- csvfile.R
+++ csvfile.R
@@ -2,7 +2,7 @@ data <- read.csv("2_4 IT.csv")
 data <- read.csv("2_4 IT.csv")
 head(data)
 tail(data)
-regular <- data[66:129, ]
+regular <- data[66:129]
 print(regular)
-lateral <- data[200:207, ]
+lateral <- data[200:207]
 print(lateral)
