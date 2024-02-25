51dd93ff296997311080da0348859bf0215b3edd
sahithmeesala <129570480+sahithmeesala@users.noreply.github.com>
2024-02-25 11:40:34 +0530

    Update WEEK3-TASK2.R

--- WEEK3-TASK2.R
+++ WEEK3-TASK2.R
@@ -34,5 +34,8 @@ print(array)
 array1 <- array(1:9, dim = c(3, 3))
 array2 <- array(10:18, dim = c(3, 3))
 array3 <- array(19:27, dim = c(3, 3))
-combined_array <- rbind(array1[1, , ], array2[1, , ], array3[1, , ])
+combined_array <- array(NA, dim = c(9, 3))
+combined_array[1:3, ] <- array1[1, ]
+combined_array[4:6, ] <- array2[1, ]
+combined_array[7:9, ] <- array3[1, ]
 print(combined_array)
